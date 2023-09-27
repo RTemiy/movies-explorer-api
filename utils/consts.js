5eed0473f7c8641191d37f1df9623b609e5b1b59
RTemiy <RTemiyGo@yandex.ru>
2023-09-27 14:17:38 +0300

    added base functionality

--- utils/consts.js
+++ utils/consts.js
@@ -1,11 +1,10 @@
 const URL_REGEXP = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
 
 const CORS = [
-  'http://',
-  'https://',
-  'http://api.',
-  'https://api.',
-  'http://',
+  'http://158.160.32.105',
+  'https://158.160.32.105',
+  'http://api.rtemiysdiploma.nomoredomainsrocks.ru',
+  'https://api.rtemiysdiploma.nomoredomainsrocks.ru',
   'http://localhost:3000',
   'http://localhost:3001',
 ];
